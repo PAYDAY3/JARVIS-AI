1d4f9b526986d0fffd0d0da30e59e5f01307d7d4
PAYDAY3 <1484533583@qq.com>
2024-03-23 07:58:24 +0000

    删除无用代码

--- my_snowboy/snowboydecoder.py
+++ my_snowboy/snowboydecoder.py
@@ -78,19 +78,6 @@ def play_audio_file(fname=DETECT_DING):
 
 
 class HotwordDetector(object):
-    """
-    Snowboy decoder to detect whether a keyword specified by `decoder_model`
-    exists in a microphone input stream.
-
-    :param decoder_model: decoder model file path, a string or a list of strings
-    :param resource: resource file path.
-    :param sensitivity: decoder sensitivity, a float of a list of floats.
-                              The bigger the value, the more senstive the
-                              decoder. If an empty list is provided, then the
-                              default sensitivity in the model will be used.
-    :param audio_gain: multiply input volume by this factor.
-    :param apply_frontend: applies the frontend processing algorithm if True.
-    """
 
     def __init__(self, decoder_model,
                  resource=RESOURCE_FILE,
@@ -131,32 +118,6 @@ class HotwordDetector(object):
               audio_recorder_callback=None,
               silent_count_threshold=15,
               recording_timeout=100):
-        """
-        Start the voice detector. For every `sleep_time` second it checks the
-        audio buffer for triggering keywords. If detected, then call
-        corresponding function in `detected_callback`, which can be a single
-        function (single model) or a list of callback functions (multiple
-        models). Every loop it also calls `interrupt_check` -- if it returns
-        True, then breaks from the loop and return.
-
-        :param detected_callback: a function or list of functions. The number of
-                                  items must match the number of models in
-                                  `decoder_model`.
-        :param interrupt_check: a function that returns True if the main loop
-                                needs to stop.
-        :param float sleep_time: how much time in second every loop waits.
-        :param audio_recorder_callback: if specified, this will be called after
-                                        a keyword has been spoken and after the
-                                        phrase immediately after the keyword has
-                                        been recorded. The function will be
-                                        passed the name of the file where the
-                                        phrase was recorded.
-        :param silent_count_threshold: indicates how long silence must be heard
-                                       to mark the end of a phrase that is
-                                       being recorded.
-        :param recording_timeout: limits the maximum length of a recording.
-        :return: None
-        """
         self._running = True
 
         def audio_callback(in_data, frame_count, time_info, status):
